912640fa67c408647b27eec18f63e6f495523103
r-amee <russel.yasol@gmail.com>
2023-03-03 15:05:21 +0800

    fixed explorecard top rounded

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -31,7 +31,7 @@ const ExploreCard = ({
         {title}
       </h3>
     ) : (
-      <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
+      <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] rounded-t-[24px]">
         <a href={link} target="_blank" rel="noopener noreferrer">
           <div
             className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
