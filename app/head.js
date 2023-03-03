bce88603ba092bba2ac7a6a6c26ffa8564b370fc
r-amee <russel.yasol@gmail.com>
2023-03-03 19:02:22 +0800

    icon fixed

--- app/head.js
+++ app/head.js
@@ -2,7 +2,7 @@ const Head = () => (
   <>
     <title>Portfolio Madness</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="../public/logo.ico" />
+    <link rel="icon" href="get-started.png" />
   </>
 );
 
--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>Portfolio Madness</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="../public/logo.ico" />
+      <link rel="icon" href="get-started.png" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
